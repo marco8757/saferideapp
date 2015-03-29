178f866f8cb8b8af718d6894cc98197938d74393
Marco <marco.cylim@gmail.com>
2015-03-29 17:31:26 +0800

    Added Fields for Login on Launcher page

--- app/src/main/java/com/cylim/saferide/MainActivity.java
+++ app/src/main/java/com/cylim/saferide/MainActivity.java
@@ -4,14 +4,32 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Button bLogin;
+    EditText etEmail, etPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bLogin = (Button) findViewById(R.id.bMLogin);
+        etEmail = (EditText) findViewById(R.id.etMEmail);
+        etPassword = (EditText) findViewById(R.id.etMPassword);
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Login button clicked.", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
