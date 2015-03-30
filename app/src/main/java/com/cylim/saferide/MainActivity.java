90f68a17fbaab572a7e4dade18d6fcc5933eeaac
Marco <marco.cylim@gmail.com>
2015-03-30 21:16:34 +0800

    Added: login feature on MainActivity

--- app/src/main/java/com/cylim/saferide/MainActivity.java
+++ app/src/main/java/com/cylim/saferide/MainActivity.java
@@ -1,7 +1,11 @@
 package com.cylim.saferide;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -9,12 +13,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.savagelook.android.UrlJsonAsyncTask;
+
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 
 public class MainActivity extends ActionBarActivity {
 
     Button bLogin;
     EditText etEmail, etPassword;
-
+    private SharedPreferences mPreferences;
+    private final static String LOGIN_API_ENDPOINT_URL = "http://192.168.1.131:8080/api/v1/sessions.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +42,105 @@ public class MainActivity extends ActionBarActivity {
         etEmail = (EditText) findViewById(R.id.etMEmail);
         etPassword = (EditText) findViewById(R.id.etMPassword);
 
+        mPreferences = getSharedPreferences("CurrentUser", MODE_PRIVATE);
+
         bLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Login button clicked.", Toast.LENGTH_LONG).show();
+
+
+                if (etEmail.getText().toString().length() == 0 || etPassword.getText().toString().length() == 0) {
+                    // input fields are empty
+                    Toast.makeText(MainActivity.this, "Please complete all the fields",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                } else {
+                    LoginTask loginTask = new LoginTask(MainActivity.this);
+                    loginTask.setMessageLoading("Logging in...");
+                    loginTask.execute(LOGIN_API_ENDPOINT_URL);
+                }
             }
         });
+    }
+
+    private class LoginTask extends UrlJsonAsyncTask {
+        public LoginTask(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... urls) {
+            DefaultHttpClient client = new DefaultHttpClient();
+            HttpPost post = new HttpPost(urls[0]);
+            JSONObject holder = new JSONObject();
+            JSONObject userObj = new JSONObject();
+            String response = null;
+            JSONObject json = new JSONObject();
+
+            try {
+                try {
+                    // setup the returned values in case
+                    // something goes wrong
+                    json.put("success", false);
+                    json.put("info", "Something went wrong. Retry!");
+                    // add the user email and password to
+                    // the params
+                    userObj.put("email", etEmail.getText().toString());
+                    userObj.put("password", etPassword.getText().toString());
+                    holder.put("user", userObj);
+                    StringEntity se = new StringEntity(holder.toString());
+                    post.setEntity(se);
+
+                    // setup the request headers
+                    post.setHeader("Accept", "application/json");
+                    post.setHeader("Content-Type", "application/json");
+
+                    ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                    response = client.execute(post, responseHandler);
+                    json = new JSONObject(response);
+
+                } catch (HttpResponseException e) {
+                    e.printStackTrace();
+                    Log.e("ClientProtocol", "" + e);
+                    json.put("info", "Email and/or password are invalid. Retry!");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e("IO", "" + e);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.e("JSON", "" + e);
+            }
+
+            return json;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            try {
+                if (json.getBoolean("success")) {
+                    // everything is ok
+                    SharedPreferences.Editor editor = mPreferences.edit();
+                    // save the returned auth_token into
+                    // the SharedPreferences
+                    editor.putString("AuthToken", json.getJSONObject("data").getString("auth_token"));
+                    editor.commit();
+
+                    // launch the HomeActivity and close this one
+                    Intent intent = new Intent(getApplicationContext(), Dashboard.class);
+                    startActivity(intent);
+                    finish();
+                }
+                Toast.makeText(MainActivity.this, json.getString("info"), Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                // something went wrong: show a Toast
+                // with the exception message
+                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+            } finally {
+                super.onPostExecute(json);
+            }
+        }
     }
 
 
