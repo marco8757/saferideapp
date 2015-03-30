9799948a4d42525dfd5b46a4f649d85cc8fd3a91
Marco <marco.cylim@gmail.com>
2015-03-30 21:13:42 +0800

    Added Dashboard activity for after login page

---
+++ app/src/main/java/com/cylim/saferide/Dashboard.java
@@ -0,0 +1,16 @@
+package com.cylim.saferide;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by marco on 30/3/15.
+ */
+public class Dashboard extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+}
