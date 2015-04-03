d6a5f942a7a1beed77d9424fb64ceca24b6d1d87
Marco <marco.cylim@gmail.com>
2015-04-03 10:08:33 +0800

    Added Splash screen. Updated:Android manifest to set Splash.java as launcher

---
+++ app/src/main/java/com/cylim/saferide/Splash.java
@@ -0,0 +1,51 @@
+package com.cylim.saferide;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+/**
+ * Created by marco on 3/4/15.
+ */
+public class Splash extends Activity {
+
+    SharedPreferences mPreferences;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        mPreferences = getSharedPreferences("CurrentUser", MODE_PRIVATE);
+
+
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    String token = mPreferences.getString("AuthToken","");
+                    Intent i = new Intent(Splash.this,MainActivity.class);
+
+                    if (token.length() > 0 ){
+                       i = new Intent(Splash.this,Dashboard.class);
+                    }
+
+                    startActivity(i);
+                }
+            }
+        };
+        timer.start();
+
+    }
+}
