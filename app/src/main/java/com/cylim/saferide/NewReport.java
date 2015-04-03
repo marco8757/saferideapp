4a7526696cd52429f133aa7c686fea87cbed7f0d
Marco <marco.cylim@gmail.com>
2015-04-03 11:30:31 +0800

    Added: New Report and Profile pages. Updated: Android Manifest

---
+++ app/src/main/java/com/cylim/saferide/NewReport.java
@@ -0,0 +1,15 @@
+package com.cylim.saferide;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by marco on 3/4/15.
+ */
+public class NewReport extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_report);
+    }
+}
---
+++ app/src/main/java/com/cylim/saferide/Profile.java
@@ -0,0 +1,15 @@
+package com.cylim.saferide;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by marco on 3/4/15.
+ */
+public class Profile extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+}
