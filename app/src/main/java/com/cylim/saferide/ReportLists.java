7404538484d34e9cab3af098bf98ee4889e4f325
Marco <marco.cylim@gmail.com>
2015-04-04 12:21:59 +0800

    Updated: ReportLists to retrieve URL of specific reports. Added list onitemclick listener and put URL as bundle for ReportDetails page.

--- app/src/main/java/com/cylim/saferide/ReportLists.java
+++ app/src/main/java/com/cylim/saferide/ReportLists.java
@@ -2,7 +2,11 @@ package com.cylim.saferide;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -22,6 +26,8 @@ public class ReportLists extends Activity {
 
     private static final String REPORTS_URL = "http://192.168.1.131:8080/reports.json";
     ListView lvReport;
+
+    List<String> reportURL;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,24 +55,39 @@ public class ReportLists extends Activity {
                 List<String> reportID = new ArrayList<String>(length);
                 List<String> reportLat = new ArrayList<String>(length);
                 List<String> reportLng = new ArrayList<String>(length);
+                reportURL = new ArrayList<String>(length);
 
 
                 for (int i = 0; i < length; i++) {
                     reportID.add(jsonReports.getJSONObject(i).getString("id"));
                     reportLat.add(jsonReports.getJSONObject(i).getString("defects_lat"));
                     reportLng.add(jsonReports.getJSONObject(i).getString("defects_lng"));
+                    reportURL.add(jsonReports.getJSONObject(i).getString("url"));
                 }
-
 
                 if (lvReport != null) {
                     lvReport.setAdapter(new ArrayAdapter<String>(ReportLists.this,
                             android.R.layout.simple_list_item_1, reportID));
+
                 }
             } catch (Exception e) {
                 Toast.makeText(context, e.getMessage(),
                         Toast.LENGTH_LONG).show();
             } finally {
                 super.onPostExecute(json);
+
+
+
+                lvReport.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                        Intent i = new Intent(ReportLists.this,ReportDetails.class);
+                        i.putExtra("ReportURL", reportURL.get((int) id));
+                        startActivity(i);
+                    }
+                });
+
             }
         }
     }
