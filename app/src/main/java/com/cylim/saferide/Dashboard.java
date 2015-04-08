2f0cbf72a1ff7ccafcbe69eae2eb4aafc2f5fa2a
Marco <marco.cylim@gmail.com>
2015-04-09 00:27:31 +0800

    Updated: dashboard to allow camera upload to backend application. Added: http libraries.

--- app/src/main/java/com/cylim/saferide/Dashboard.java
+++ app/src/main/java/com/cylim/saferide/Dashboard.java
@@ -1,18 +1,32 @@
 package com.cylim.saferide;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
+import com.savagelook.android.UrlJsonAsyncTask;
+
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by marco on 30/3/15.
@@ -20,8 +34,12 @@ import java.io.IOException;
 public class Dashboard extends Activity implements View.OnClickListener {
 
     private static final int CAMERA_PICTURE = 1337;
+    private final String reportURL = "http://192.168.1.131:8080/api/v1/reports.json";
     ImageView ivNewReport, ivViewReport, ivProfile;
     TextView tvNewReport, tvViewReport, tvProfile;
+    Bitmap thumbnail;
+    GPSTagger gps;
+    double lat, lng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,32 +89,121 @@ public class Dashboard extends Activity implements View.OnClickListener {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAMERA_PICTURE){
+        if (requestCode == CAMERA_PICTURE) {
             try {
                 Toast.makeText(Dashboard.this, "CAMERA LAUNCHED", Toast.LENGTH_LONG).show();
-                Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+                thumbnail = (Bitmap) data.getExtras().get("data");
                 Log.d("ImageRaw", data.getExtras().get("data").toString());
-                //post thumbnail to server
                 ivNewReport.setImageBitmap(thumbnail);
-                GPSTagger gps = new GPSTagger(Dashboard.this);
+                gps = new GPSTagger(Dashboard.this);
 
                 if (gps.canGetLocation()) {
 
-                    double latitude = gps.getLatitude();
-                    double longitude = gps.getLongitude();
+                    lat = gps.getLatitude();
+                    lng = gps.getLongitude();
 
-                    Log.d("GPSTagger Location", latitude + " " + longitude);
+                    Log.d("GPSTagger Location", lat + " " + lng);
+
+                    NewReport nr = new NewReport(Dashboard.this);
+                    nr.setMessageLoading("Uploading picture...");
+                    nr.execute(reportURL);
+
                 } else {
                     gps.showSettingsAlert();
                 }
 
-            }catch (RuntimeException e){
+
+            } catch (RuntimeException e) {
                 Log.d("Dashboard", e.toString());
                 Toast.makeText(Dashboard.this, "Oops, failed to capture picture.", Toast.LENGTH_LONG).show();
             }
 
         }
+
     }
 
+    public static String encodeTobBase64(Bitmap image) {
+
+        if (image == null)
+            return null;
+
+        Bitmap bm = image;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+
+        byte[] b = baos.toByteArray();
+
+        String imageEncoded = Base64.encodeToString(b, Base64.DEFAULT);
+
+        return imageEncoded;
+    }
+
+    private class NewReport extends UrlJsonAsyncTask {
+        public NewReport(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... urls) {
+            DefaultHttpClient client = new DefaultHttpClient();
+            HttpPost post = new HttpPost(urls[0]);
+            JSONObject holder = new JSONObject();
+            JSONObject reportObj = new JSONObject();
+            String response = null;
+            JSONObject json = new JSONObject();
+
+            try {
+                try {
+                    json.put("success", false);
+                    json.put("info", "Something went wrong. Retry!");
+
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ZZZZZ");
+                    String currentTimeStamp = dateFormat.format(new Date());
+
+                    reportObj.put("picture", "data:image/jpg;base64,("+ encodeTobBase64(thumbnail) + ")");
+                    reportObj.put("defects_lat", lat);
+                    reportObj.put("defects_lng", lng);
+                    reportObj.put("report_time", currentTimeStamp);
+                    holder.put("report", reportObj);
+                    StringEntity se = new StringEntity(holder.toString());
+                    post.setEntity(se);
+
+                    // setup the request headers
+                    post.setHeader("Accept", "application/json");
+                    post.setHeader("Content-Type", "application/json");
+
+                    ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                    response = client.execute(post, responseHandler);
+                    json = new JSONObject(response);
+
+                } catch (HttpResponseException e) {
+                    e.printStackTrace();
+                    Log.e("ClientProtocol", "" + e);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e("IO", "" + e);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.e("JSON", "" + e);
+            }
+
+            return json;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            try {
+                if (json.getBoolean("success")) {
+                    Toast.makeText(Dashboard.this, "Successfully submitted your report.", Toast.LENGTH_LONG).show();
+                }
+
+            } catch (Exception e) {
+                Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+            } finally {
+                super.onPostExecute(json);
+            }
+        }
+    }
 
 }
