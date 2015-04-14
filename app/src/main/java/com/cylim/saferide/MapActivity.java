43c82d6618838890565029e91d737a8304a97887
Marco <marco.cylim@gmail.com>
2015-04-14 15:58:46 +0800

    Updated: Map Activity to actionbaractivity with action buttons for navigations.

--- app/src/main/java/com/cylim/saferide/MapActivity.java
+++ app/src/main/java/com/cylim/saferide/MapActivity.java
@@ -2,7 +2,10 @@ package com.cylim.saferide;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,7 +17,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 /**
  * Created by marco on 12/4/15.
  */
-public class MapActivity extends Activity  implements OnMapReadyCallback {
+public class MapActivity extends ActionBarActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,5 +56,31 @@ public class MapActivity extends Activity  implements OnMapReadyCallback {
                 .title("It's me here.")
                 .snippet("This is where I am now.")
                 .position(mylocation));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_new_report:
+                break;
+            case R.id.action_profile:
+                break;
+            case R.id.action_logout:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
