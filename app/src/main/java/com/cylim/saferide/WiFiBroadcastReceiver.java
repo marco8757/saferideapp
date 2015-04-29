88230d72aa8de31e8c497e001177193a9917beee
Marco <marco.cylim@gmail.com>
2015-04-29 21:36:39 +0800

    Added: WiFiBroadcastReceiver to detect wifi connection status.

---
+++ app/src/main/java/com/cylim/saferide/WiFiBroadcastReceiver.java
@@ -0,0 +1,23 @@
+package com.cylim.saferide;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+/**
+ * Created by marco on 29/4/15.
+ */
+public class WiFiBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ConnectivityManager connMan = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMan.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        final boolean isWifiConn = networkInfo.isConnected();
+        //
+        Log.d("WiFi connected", String.valueOf(isWifiConn));
+    }
+}
