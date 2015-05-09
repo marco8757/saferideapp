45c5dcf0aa78a4da6ae49017cdef5bf5e562bb9c
Marco <marco.cylim@gmail.com>
2015-05-09 18:32:31 +0800

    Updated: Profile activity with asynctask for updating profile information and saving into local sharedpreference after updated on server.

--- app/src/main/java/com/cylim/saferide/Profile.java
+++ app/src/main/java/com/cylim/saferide/Profile.java
@@ -1,16 +1,31 @@
 package com.cylim.saferide;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.savagelook.android.UrlJsonAsyncTask;
 import com.squareup.picasso.Picasso;
 
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -24,7 +39,7 @@ public class Profile extends Activity {
     ImageView ivProfilePicture;
     Boolean editMode = false;
     private SharedPreferences mPreferences;
-
+    private final static String PROFILE_ENDPOINT = "http://saferidebymarco.herokuapp.com/api/v1/update_profile.json";
     String emailHash;
 
     @Override
@@ -46,15 +61,17 @@ public class Profile extends Activity {
 
         emailHash = md5Hash(mPreferences.getString("Email", "").trim());
 
-        if (!emailHash.contains("null")){
+        if (!emailHash.contains("null")) {
             Picasso.with(Profile.this).load("http://www.gravatar.com/avatar/" + emailHash + "?s=200").into(ivProfilePicture);
         }
-
 
 
         bUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
 
                 if (editMode == false) {
                     editMode = true;
@@ -63,7 +80,12 @@ public class Profile extends Activity {
                     etName.setVisibility(View.VISIBLE);
                     tvName.setVisibility(View.GONE);
                 } else {
-                    editModeOff();
+                    if (etName.getText().toString().length() > 0) {
+                        UpdateProfile updateProfile = new UpdateProfile(Profile.this);
+                        updateProfile.execute(PROFILE_ENDPOINT);
+                    } else {
+                        Toast.makeText(Profile.this, "Please fill in your name to continue.", Toast.LENGTH_LONG).show();
+                    }
                 }
             }
         });
@@ -71,6 +93,8 @@ public class Profile extends Activity {
         bCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
                 editModeOff();
             }
         });
@@ -102,5 +126,76 @@ public class Profile extends Activity {
         bCancel.setVisibility(View.GONE);
         etName.setVisibility(View.GONE);
         tvName.setVisibility(View.VISIBLE);
+    }
+
+
+    private class UpdateProfile extends UrlJsonAsyncTask {
+
+        public UpdateProfile(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... urls) {
+            DefaultHttpClient client = new DefaultHttpClient();
+            HttpPost post = new HttpPost(urls[0]);
+            JSONObject holder = new JSONObject();
+            JSONObject profileObj = new JSONObject();
+            String response = null;
+            JSONObject json = new JSONObject();
+
+            try {
+                try {
+                    json.put("success", false);
+                    json.put("info", "Something went wrong. Retry!");
+
+                    profileObj.put("authentication_token", mPreferences.getString("AuthToken", ""));
+                    profileObj.put("id", mPreferences.getString("UserID", ""));
+                    profileObj.put("name", etName.getText().toString());
+                    holder.put("user", profileObj);
+                    StringEntity se = new StringEntity(holder.toString());
+                    post.setEntity(se);
+
+                    // setup the request headers
+                    post.setHeader("Accept", "application/json");
+                    post.setHeader("Content-Type", "application/json");
+
+                    ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                    response = client.execute(post, responseHandler);
+                    json = new JSONObject(response);
+
+                } catch (HttpResponseException e) {
+                    e.printStackTrace();
+                    Log.e("ClientProtocol", "" + e);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e("IO", "" + e);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.e("JSON", "" + e);
+            }
+
+            return json;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            try {
+                if (json.getBoolean("success")) {
+                    editModeOff();
+                    tvName.setText(etName.getText().toString());
+
+                    SharedPreferences.Editor editor = mPreferences.edit();
+                    editor.putString("Username", etName.getText().toString());
+                    editor.commit();
+                }
+
+            } catch (Exception e) {
+                Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+            } finally {
+                super.onPostExecute(json);
+            }
+        }
     }
 }
