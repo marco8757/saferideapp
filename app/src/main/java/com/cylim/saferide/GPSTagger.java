249596bfd812fafdfabfdc76348f1c3ec9736911
Marco <marco.cylim@gmail.com>
2015-05-09 15:03:33 +0800

    Updated: GPSTagger to update location by seconds and update changes of lat lng.

--- app/src/main/java/com/cylim/saferide/GPSTagger.java
+++ app/src/main/java/com/cylim/saferide/GPSTagger.java
@@ -36,7 +36,7 @@ public class GPSTagger extends Service implements LocationListener {
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
 
     // The minimum time between updates in milliseconds
-    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+    private static final long MIN_TIME_BW_UPDATES = 1000; // 1 second
 
     // Declaring a Location Manager
     protected LocationManager locationManager;
@@ -155,7 +155,8 @@ public class GPSTagger extends Service implements LocationListener {
 
     @Override
     public void onLocationChanged(Location location) {
-
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
     }
 
     @Override
